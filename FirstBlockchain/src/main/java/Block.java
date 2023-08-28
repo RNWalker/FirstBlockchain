ff3c287f3b85988f26e797a6fb10d39f9cd4ffe2
RNWalker <rebecca.noelia.walker@gmail.com>
2023-08-28 17:56:43 +0100

    first commit

---
+++ FirstBlockchain/src/main/java/Block.java
@@ -0,0 +1,26 @@
+import java.util.Date;
+public class Block {
+
+    public String hash;
+    public String previousHash;
+    public String data;
+
+    private long timeStamp;
+
+
+    public Block (String data, String previousHash){
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+        this.hash = calculateHash(); //make sure to do this after setting other values
+    }
+
+    public String calculateHash(){
+        String calculatedHash = StringUtil.applySha256(
+                previousHash +
+                        Long.toString(timeStamp) +
+                        data
+        );
+        return calculatedHash;
+    }
+}
